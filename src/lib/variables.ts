578e69d23790ff9f9fa9eaeb003ab34e678af8e6
Tayfun Gülcan <tayfunmobile@googlemail.com>
2022-03-21 14:21:36 +0100

    fix: add process env

--- src/lib/variables.ts
+++ src/lib/variables.ts
@@ -1,4 +1,4 @@
 export const variables = {
 	movieUrl: 'https://api.themoviedb.org/3',
-	movieToken: import.meta.env.VITE_MOVIE_TOKEN
+	movieToken: import.meta.env.VITE_MOVIE_TOKEN || process.env.VITE_MOVIE_TOKEN
 };
